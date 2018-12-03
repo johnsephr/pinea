9ba9c91ac0bece6d60ac7f7f2b751acf3617b4d0
John Ramsey <dextrodeath@Johns-MacBook-Pro.local>
2018-12-02 17:13:07 -0700

    NFC Manager Bug Fixes

--- App/Containers/ScanScreen.js
+++ App/Containers/ScanScreen.js
@@ -60,20 +60,7 @@ class ScanScreen extends Component {
 
     NfcManager.start({
       onSessionClosedIOS: () => {
-        // console.log('ios session closed');
-
-        Alert.alert(
-          'Error',
-          'onSessionClosedIOS',
-          [
-            // { text: 'View Products', onPress: () => { this.props.navigation.navigate('Products') } },
-            { text: 'OK', onPress: () => console.log('OK Pressed') }
-          ],
-          {
-            cancelable: false
-          }
-        )
-
+        console.log('ios session closed');
         NfcManager.unregisterTagEvent()
       }
     })
